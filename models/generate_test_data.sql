9cd387255e440b192175ad5c433312b360bdd1fb
Zee <ziyi.li@bytedance.com>
2024-06-04 13:28:16 -0700

    version1

--- models/generate_test_data.sql
+++ models/generate_test_data.sql
@@ -1,2 +1,19 @@
 -- :dbt run --models generate_test_data
 
+WITH base_data AS (
+  SELECT
+    '{{ dbt_utils.surrogate_key(["id"]) }}' AS id,
+    '{{ dbt_utils.random_int(100, 999) }}' AS random_number,
+    '{{ dbt_utils.random_date("2022-01-01", "2022-12-31") }}' AS random_date,
+    '{{ dbt_utils.random_choice(["A", "B", "C"]) }}' AS category
+  FROM
+    {{ dbt_utils.generate_series(1, 1000) }} -- 生成1000条记录
+)
+
+SELECT
+  id,
+  random_number,
+  random_date,
+  category
+FROM
+  base_data
